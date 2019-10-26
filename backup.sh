9a46b43c14dea24a779f0a58a8af91cde8823047
Russ MacShane <rmacshane@liquidweb.com>
2019-10-26 14:37:32 -0500

    Working backup and restores

--- backup.sh
+++ backup.sh
@@ -3,14 +3,11 @@
 source /quick-wordpress/config
 
 echo "***************************** Backup Database ****************************"
-sudo mysqldump $wp_db > $wp_db-datadump.sql
-# mysql db_name < backup-file.sql (restore)
-# https://mariadb.com/kb/en/library/mysqldump/
+sudo mysqldump $wp_db > datadump.sql
 
 echo "***************************** Backups Files ******************************"
 mkdir -p /quick-wordpress/backup
-tar -cjvf /quick-wordpress/backup/$(date '+%F_%s').tar.bz2 /var/www/html $wp_db-datadump.sql
-# https://www.howtogeek.com/248780/how-to-compress-and-extract-files-using-the-tar-command-on-linux/
+tar -cjvf /quick-wordpress/backup/$(date '+%F_%s').tar.bz2 /var/www/html datadump.sql
 
 echo "***************************** Cleanup ************************************"
-rm $wp_db-datadump.sql
+rm datadump.sql
---
+++ restore.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source /quick-wordpress/config
+
+echo "***************************** WordPress Restore **************************"
+read -p "Enter the filename of the backup you would like to restore: " file
+
+backup_file=/quick-wordpress/backup/$file
+
+if [ ! -f $backup_file ]; then
+    echo "$backup_file not found!" 1>&2
+    exit 1
+fi
+
+echo "***************************** Unpack Backup ******************************"
+mkdir -p /tmp/wp-bkup
+tar -xjvf $backup_file -C /tmp/wp-bkup
+
+echo "***************************** Nuke WP Installation ***********************"
+sudo rm -rf /var/www/html/*
+
+echo "***************************** Restore Files ******************************"
+sudo mv /tmp/wp-bkup/var/www/html/* /var/www/html/
+
+echo "***************************** Restore Database ***************************"
+sudo mysql $wp_db < /tmp/wp-bkup/datadump.sql
+
+echo "***************************** Cleanup ************************************"
+rm -rf /tmp/wp-bkup
